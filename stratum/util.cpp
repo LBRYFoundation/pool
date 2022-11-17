ad8ee94a660778aa7cd83d3e3c200c39873f9dd4
Ralph <blackbirdralph@aol.com>
2022-11-17 12:24:42 -0500

    Correct message

--- stratum/util.cpp
+++ stratum/util.cpp
@@ -189,7 +189,7 @@ void lbrylog(const char *format, ...)
 
 	char message[YAAMP_SMALLBUFSIZE+1024];
 	sprintf(message, "{\"username\":\"stratum\", \"content\":\"%s: %s\"}", buffer2, buffer);
-	discordlog(messages);
+	discordlog(message);
 }
 
 void discordlog(const char *message)
